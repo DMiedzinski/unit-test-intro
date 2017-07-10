1465fb826cbe08ea6d1b99d24ea2e0467ba36930
DMiedzinski <daniel.miedzinski@gmail.com>
2017-07-10 19:56:09 +0200

    My first test in Java

---
+++ src/main/java/com/example/task01/FibonacciTest.java
@@ -0,0 +1,32 @@
+package com.example.task01;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class FibonacciTest {
+
+	@Test
+	public void testFibonacci() {
+		Fibonacci fib = new Fibonacci();
+		int test = fib.fibonacci(6);
+		int results = 8;
+		
+		Assert.assertEquals(results, test);
+	}
+	
+	public void shouldReturnOneForTwo() {
+		Fibonacci fib = new Fibonacci();
+		int test = fib.fibonacci(2);
+		int results = 1;
+		
+		Assert.assertEquals(results, test);
+
+}
+	public void shouldReturn34For9(){
+		Fibonacci fib = new Fibonacci();
+		int test = fib.fibonacci(9);
+		int result = 34;
+		Assert.assertEquals(result, test);
+	}
+}
